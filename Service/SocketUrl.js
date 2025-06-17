3a3fd5b010e89e48e4019ee6d42e9ab6c8411c53
harshendra raj <harsendraraj20@gmail.com>
2025-06-17 13:34:17 +0530

    BaseUrl updated

--- Service/SocketUrl.js
+++ Service/SocketUrl.js
@@ -1,5 +1,5 @@
-// const SocketUrl = 'http://13.127.177.57';
+const SocketUrl = 'http://13.233.163.118';
 
-const SocketUrl = 'http://localhost:4000';
+// const SocketUrl = 'http://localhost:4000';
 
 export default SocketUrl;
