0a49a821312031649ee632bb19e378cb5802e708
harshendra2 <harsendraraj20@gmail.com>
2025-06-24 20:36:18 +0530

    next file configration fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 const nextConfig = {
   images: {
-    domains: ['13.201.85.157', 'localhost', '127.0.0.1',"https://chat-karo-server.onrender.com"]
+    domains: ['13.201.85.157', 'localhost', '127.0.0.1',"chat-karo-server.onrender.com"]
   },
 };
+
 
 export default nextConfig;
 
